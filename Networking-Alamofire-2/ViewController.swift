ae472b1ddb28ed0dcd62518e61980f6fdaf14cf4
yeonior <81576794+yeonior@users.noreply.github.com>
2022-06-16 23:22:41 +0500

    Initial Commit

---
+++ Networking-Alamofire-2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Networking-Alamofire-2
+//
+//  Created by Ruslan on 16.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

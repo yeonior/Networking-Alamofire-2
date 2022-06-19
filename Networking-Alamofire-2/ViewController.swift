e12568bf4df57edb021c5e1cdaba68b002b6336e
yeonior <81576794+yeonior@users.noreply.github.com>
2022-06-19 19:42:21 +0500

    Updated ViewController

--- Networking-Alamofire-2/ViewController.swift
+++ Networking-Alamofire-2/ViewController.swift
@@ -7,13 +7,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    // MARK: - Subviews
+    private let myImageView: UIImageView = {
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    // MARK: - Properties
+    private let imageURL = URL(string: "https://images.unsplash.com/photo-1554960717-a66eb28aef80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2071&q=80")!
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureUI()
+        requestImage()
     }
 
-
+    // MARK: - Methods
+    private func configureUI() {
+        view.addSubview(myImageView)
+        myImageView.center = view.center
+    }
+    
+    private func requestImage() {
+        NetworkManager.shared.downloadImage(imageURL) { [weak self] image in
+            self?.myImageView.image = image
+        }
+    }
 }
 
